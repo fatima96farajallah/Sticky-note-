fdbf21c395c032a1bdf9c5a1216f3d00d2b5bb5a
fatima farajallah <farajallahmfatima@gmail.com>
2019-10-30 10:27:04 +0200

    www

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 
-
+class Note {
+  constructor(id , description , color)
+  {
+    this.id=id;
+    this.description=description;
+    this.color=color;
+  }
+}
 let Notsestorge = JSON.parse(localStorage.getItem('arr')) || [];
 let colors = {color1:'#d6eadf', 
 color2 :'#b8e0d2',
  color3 :'#95b8d1',
   color4 :'#809bce'}
 let numnote = 0;
-let arr = [];
 let notes = document.getElementById("notes");
 
 function closeNote(note) {
@@ -14,22 +20,24 @@ function closeNote(note) {
 }
 
 function newNote() {
+  let description = document.getElementById('note${numnote}');
+
   numnote++;
-  Notsestorge.push(numnote);
+  Notsestorge.push(description);
   let Note = document.createElement("div");
   Note.innerHTML = addnote(numnote);
   localStorage.setItem('arr',JSON.stringify(Notsestorge));
   Notsestorge=JSON.parse(localStorage.getItem('arr'));
-  console.log(Notsestorge);
+  console.log(description);
   addtoborde(Note);
 }
 
 function addnote(numnote) {
-  let top = Math.floor(Math.random() * Math.floor(1000));
+  let top = Math.floor(Math.random() * Math.floor(700));
   let left = Math.floor(Math.random() * Math.floor(1000));
 
   let note = ` <div class="note" id="note" style="top: ${top}px; left: ${left}px">
-      <textarea id="note${numnote}"> </textarea>    
+      <textarea id="note${numnote}">hjghj</textarea>    
         <div class="noteColor">
           <button class="btn1" onclick="changecolor('color1', this.parentElement.parentElement)" > </button>
           <button class="btn2" onclick="changecolor('color2', this.parentElement.parentElement)"> </button>
